2b42ae8467af6a119a1602954ed07e37700eab4b
Dimonka2 <dimonka@gmail.com>
2019-12-09 11:56:36 +0100

    composer

---
+++ src/PlatformServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace dimonka2\platform;
+
+use Illuminate\Support\ServiceProvider;
+
+class PlatformServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
