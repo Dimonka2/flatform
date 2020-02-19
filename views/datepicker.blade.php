211b28676471b5acce7ee7c262c73c77dc8989e5
dimonka2 <dimonka2@gmail.com>
2020-02-19 19:46:47 +0100

    datepicker init

--- views/datepicker.blade.php
+++ views/datepicker.blade.php
@@ -1,9 +1,16 @@
 @push($context->getJsStack())
     <script>
-        $(document).ready(function () {
-            $('.datepicker').datepicker({
-            dateFormat: '{{ config('flatform.form.date_format_js') }}'
+        function initDatepickers(){
+            $('.datepicker:not(.datepicker-enabled)').each(function(i, obj) {
+                $(obj).addClass('datepicker-enabled');
+                $(obj).datepicker({
+                    dateFormat: '{{ config('flatform.form.date_format_js') }}'
+                });
             });
+        }
+
+        $(document).ready(function () {
+            initDatepickers();
         });
     </script>
 @endpush
