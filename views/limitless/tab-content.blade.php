df660e7c6d1b314409f91ea29c7cb760a6f94ec6
dimonka2 <dimonka2@gmail.com>
2020-06-23 18:55:18 +0200

    tab content

--- views/limitless/tab-content.blade.php
+++ views/limitless/tab-content.blade.php
@@ -1,4 +1,4 @@
-<div class="block-content block-content-full tab-content px-md-5" style="min-height: 303px;">
+<div class="block-content block-content-full tab-content px-md-5">
     @foreach ($element as $tab)
         @if(!$tab->getHidden())
         <div class="tab-pane {{$element->activeID == $tab->id ? 'active' : '' }}"  id="{{ $tab->id }}" role="tabpanel">
