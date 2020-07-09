d0228b8f07f5244ea102ae185fea53df795191cc
Dimonka2 <dimonka@gmail.com>
2020-07-09 18:06:51 +0200

    breadcrumb views

--- views/bootstrap/breadcrumbs.blade.php
+++ views/bootstrap/breadcrumbs.blade.php
@@ -1,21 +1,14 @@
 <nav class="flex-sm-00-auto" aria-label="breadcrumb">
     <ol class="breadcrumb{{isset($element->class) ? ' ' . $element->class : ''}} ">
         @foreach($element->items as $breadcrub)
+        @php($hasUrl = $breadcrub['url'] ?? false)
         <li class="breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
-            <a class="link-fx" href="{{$breadcrub['url'] ?? '#'}}"{!!
-                isset($breadcrub['target']) ? ' target="'. $breadcrub['target'] . '"' : ''  !!}>
-                {!! isset($breadcrub['icon']) ? '<i class="' . $breadcrub['icon'] . '"></i>' : '' !!}
-                {!! $breadcrub['title'] !!}
-            </a>
+            @form([$breadcrumb])
         </li>
         @endforeach
         @foreach($element->nav_right ?? [] as $breadcrub)
         <li class="{{$loop->first ? 'ml-auto ' : ''}}breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
-            <a class="link-fx" href="{{$breadcrub['url'] ?? '#'}}"{!!
-                isset($breadcrub['target']) ? ' target="'. $breadcrub['target'] . '"' : ''  !!}>
-                {!! isset($breadcrub['icon']) ? '<i class="' . $breadcrub['icon'] . '"></i>' : '' !!}
-                {!! $breadcrub['title'] !!}
-            </a>
+            @form([$breadcrumb])
         </li>
         @endforeach
     </ol>
--- views/metronic/breadcrumbs.blade.php
+++ views/metronic/breadcrumbs.blade.php
@@ -1,23 +1,15 @@
 <div class="kt-portlet p-0" aria-label="breadcrumb">
     <div class='kt-portlet__body p-0'>
         <nav>
-        <ol class="breadcrumb bg-white m-0{{isset($element->class) ? ' ' . $element->class : ''}} ">
-            @foreach($element->items as $breadcrub)
-            <li class="breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
-                <a class="link-fx" href="{{$breadcrub['url'] ?? '#'}}"{!!
-                    isset($breadcrub['target']) ? ' target="'. $breadcrub['target'] . '"' : ''  !!}>
-                    {!! isset($breadcrub['icon']) ? '<i class="' . $breadcrub['icon'] . '"></i>' : '' !!}
-                    {!! $breadcrub['title'] !!}
-                </a>
+        <ol class="breadcrumb m-0{{isset($element->class) ? ' ' . $element->class : ''}} ">
+            @foreach($element->items as $breadcrumb)
+            <li class="breadcrumb-item{{isset($breadcrumb['active']) ? ' active"  aria-current="page' : ''}}">
+                @form([$breadcrumb])
             </li>
             @endforeach
-            @foreach($element->nav_right ?? [] as $breadcrub)
-            <li class="{{$loop->first ? 'ml-auto ' : ''}}breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
-                <a class="link-fx" href="{{$breadcrub['url'] ?? '#'}}"{!!
-                    isset($breadcrub['target']) ? ' target="'. $breadcrub['target'] . '"' : ''  !!}>
-                    {!! isset($breadcrub['icon']) ? '<i class="' . $breadcrub['icon'] . '"></i>' : '' !!}
-                    {!! $breadcrub['title'] !!}
-                </a>
+            @foreach($element->nav_right ?? [] as $breadcrumb)
+            <li class="{{$loop->first ? 'ml-auto ' : ''}}breadcrumb-item{{isset($breadcrumb['active']) ? ' active"  aria-current="page' : ''}}">
+                @form([$breadcrumb])
             </li>
             @endforeach
         </ol>
