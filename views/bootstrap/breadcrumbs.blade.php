d10f849069596c20db84fb4b5efa1dc3637b4a67
dimonka2 <dimonka2@gmail.com>
2020-07-15 00:37:52 +0200

    fix bredcrumbs for bootstrap

--- views/bootstrap/breadcrumbs.blade.php
+++ views/bootstrap/breadcrumbs.blade.php
@@ -1,13 +1,13 @@
 <nav class="flex-sm-00-auto" aria-label="breadcrumb">
     <ol class="breadcrumb{{isset($element->class) ? ' ' . $element->class : ''}} ">
-        @foreach($element->items as $breadcrub)
-        @php($hasUrl = $breadcrub['url'] ?? false)
-        <li class="breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
+        @foreach($element->items as $breadcrumb)
+        @php($hasUrl = $breadcrumb['url'] ?? false)
+        <li class="breadcrumb-item{{isset($breadcrumb['active']) ? ' active"  aria-current="page' : ''}}">
             @form([$breadcrumb])
         </li>
         @endforeach
-        @foreach($element->nav_right ?? [] as $breadcrub)
-        <li class="{{$loop->first ? 'ml-auto ' : ''}}breadcrumb-item{{isset($breadcrub['active']) ? ' active"  aria-current="page' : ''}}">
+        @foreach($element->nav_right ?? [] as $breadcrumb)
+        <li class="{{$loop->first ? 'ml-auto ' : ''}}breadcrumb-item{{isset($breadcrumb['active']) ? ' active"  aria-current="page' : ''}}">
             @form([$breadcrumb])
         </li>
         @endforeach
